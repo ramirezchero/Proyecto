6612b14c27f051f22e68293bd785668743d9a886
amilkamus <amilcar.huaman@gmail.com>
2018-10-27 21:51:19 -0500

    Se sube proyecto que administra la generación de archivos logs.

---
+++ ProyectoFacturaElectronica/FactElec.Log/ConfiguracionAttribute.cs
@@ -0,0 +1,43 @@
+﻿using log4net.Config;
+using log4net.Repository;
+using System;
+using System.Reflection;
+
+namespace FactElec.Log
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ConfiguracionAttribute : ConfiguratorAttribute
+    {
+        private const string conversionPattern = "%date{dd-MM-yyyy HH:mm:ss,fff} %-5level %thread %logger %method %message %exception %newline";
+        private string NombreArchivo { get; set; }
+
+        public ConfiguracionAttribute(string nombreArchivo)
+            : base(0) => this.NombreArchivo = nombreArchivo;
+
+        public override void Configure(Assembly sourceAssembly, ILoggerRepository targetRepository)
+        {
+            var patternLayout = new log4net.Layout.PatternLayout
+            {
+                ConversionPattern = conversionPattern
+            };
+            patternLayout.ActivateOptions();
+
+            var roller = new log4net.Appender.RollingFileAppender
+            {
+                AppendToFile = true,
+                File = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logger"),
+                Layout = patternLayout,
+                RollingStyle = log4net.Appender.RollingFileAppender.RollingMode.Date,
+                DatePattern = $"'/'yy'/'MM'/'dd'/{NombreArchivo}_'yyyyMMdd'.log'",
+                StaticLogFileName = false,
+                LockingModel = new log4net.Appender.FileAppender.MinimalLock()
+            };
+            roller.ActivateOptions();
+
+            var hierarchy = (log4net.Repository.Hierarchy.Hierarchy)targetRepository;
+            hierarchy.Root.AddAppender(roller);
+            hierarchy.Root.Level = log4net.Core.Level.Info;
+            hierarchy.Configured = true;
+        }
+    }
+}
